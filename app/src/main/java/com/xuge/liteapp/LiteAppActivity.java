4d2f5c0b4e6d07d9ae4a86154a71457788937529
yixu.wang <yixu.wang@ihandysoft.com>
2019-03-10 14:20:15 +0800

    添加资源

---
+++ app/src/main/java/com/xuge/liteapp/LiteAppActivity.java
@@ -0,0 +1,19 @@
+package com.xuge.liteapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created at 2019/3/10 下午12:39.
+ *
+ * @author yixu.wang
+ */
+public class LiteAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lite_app);
+    }
+}
---
+++ app/src/main/java/com/xuge/liteapp/WebActivity.java
@@ -0,0 +1,11 @@
+package com.xuge.liteapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created at 2019/3/10 下午12:35.
+ *
+ * @author yixu.wang
+ */
+public class WebActivity extends AppCompatActivity {
+}
