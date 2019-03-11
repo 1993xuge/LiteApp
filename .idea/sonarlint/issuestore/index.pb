2030d46c96fef9d94537a069375491386e3a2a91
yixu.wang <yixu.wang@ihandysoft.com>
2019-03-11 20:55:31 +0800

    hhaha

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+c
+3app/src/main/java/com/xuge/liteapp/WebActivity.java,3/f/3f9aa5b8248f00dc28f821fdff6b932b9127b53d
+P
+ app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
+d
+4app/src/main/java/com/xuge/liteapp/MainActivity.java,3/2/3232f34d93feaf08c7612d165c26c83230fc6434
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+g
+7app/src/main/java/com/xuge/liteapp/LiteAppActivity.java,f/f/ffaa783129403c8b279e8062697db8b5e70fbb3b
--- app/src/main/java/com/xuge/liteapp/MainActivity.java
+++ app/src/main/java/com/xuge/liteapp/MainActivity.java
@@ -2,12 +2,14 @@ package com.xuge.liteapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -44,9 +46,55 @@ public class MainActivity extends AppCompatActivity {
         if (liteApp == null) {
             return;
         }
-        Intent intent = new Intent(this, LiteAppActivity.class);
+        /*Intent intent = new Intent(this, LiteAppActivity.class);
         intent.putExtra(LiteAppActivity.INTENT_EXTRA_LITEAPP, liteApp);
-        startActivity(intent);
+        startActivity(intent);*/
+
+        createSystemSwitcherShortCut(this, liteApp.getIcon(), liteApp.getTitleResId());
+    }
+
+
+    public static void createSystemSwitcherShortCut(Context context, int iconResId, int labelResId) {
+        final Intent addIntent = new Intent(
+                "com.android.launcher.action.INSTALL_SHORTCUT");
+        final Parcelable icon = Intent.ShortcutIconResource.fromContext(
+                context, iconResId); // 获取快捷键的图标
+        addIntent.putExtra("duplicate", false);
+        final Intent myIntent = new Intent(context,
+                LiteAppActivity.class);
+        myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME,
+                context.getString(labelResId));// 快捷方式的标题
+        addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon);// 快捷方式的图标
+        addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, myIntent);// 快捷方式的动作
+        context.sendBroadcast(addIntent);
+        Log.d("xuge", "createSystemSwitcherShortCut: ");
+    }
+
+
+    /**
+     * 为程序创建桌面快捷方式
+     */
+    private void addShortcut() {
+        Intent shortcut = new Intent("com.android.launcher.action.INSTALL_SHORTCUT");
+
+        //快捷方式的名称
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_NAME, getString(R.string.app_name));
+        shortcut.putExtra("duplicate", false); //不允许重复创建
+
+        /****************************此方法已失效*************************/
+        //ComponentName comp = new ComponentName(this.getPackageName(), "."+this.getLocalClassName());
+        //shortcut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, new Intent(Intent.ACTION_MAIN).setComponent(comp));  　　
+        /******************************end*******************************/
+        Intent shortcutIntent = new Intent(Intent.ACTION_MAIN);
+        shortcutIntent.setClassName(this, this.getClass().getName());
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+
+        //快捷方式的图标
+        Intent.ShortcutIconResource iconRes = Intent.ShortcutIconResource.fromContext(this, R.drawable.icon_weibo);
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconRes);
+
+        sendBroadcast(shortcut);
     }
 
     private class ContentAdapter extends RecyclerView.Adapter {
